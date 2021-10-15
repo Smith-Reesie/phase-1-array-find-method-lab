7502cecf2ea5d03fe9d9d301e06f918ab43f4dc9
Maurice Smith <Xmaurice.smith@gmail.com>
2021-10-15 12:14:49 -0400

    commit tree 11f5757f26f7ff51125c2ab9b55718bb9a7fa05c

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 
+// const record = [
+//     {year: "2018", result: "L"},
+//     {year: "2017", result: "W"},
+//     {year: "2016", result: "N/A"}
+//     //...
+//   ]
+
+  function superbowlWin(array) {
+   const newArray = array.find(array => array.result === "W")
+   //v if newArray = True ; happenes once we find  "find()" what its looking for 
+    if(newArray){
+        return  newArray.year 
+    }else {
+        return undefined
+    }
+  }
+
+  
+  
